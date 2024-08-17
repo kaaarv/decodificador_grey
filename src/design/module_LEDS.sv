d7fc3608c81232cc4f85dcbe1d2dfed7620f3231
Juan Pablo Elizondo Espinoza <pablo.elizondo17@hotmail.com>
2024-08-17 14:41:53 -0600

    Control de LEDs

---
+++ src/design/module_LEDS.sv
@@ -0,0 +1,14 @@
+module module_LEDS # (  //Control de los leds (recibe el código binario)
+
+    input logic [3 : 0] binary_code,    //Entrada: Código Grey ya convertido a binario
+    output logic [3 : 0] leds_code      //Salida: Código para el control de LEDs
+);
+    
+    assign leds_code = binary_code;     //Asignación: Código binario al control de LEDs
+endmodule
+
+
+
+
+
+
